60e15b0f8f9c88e18591467f1dd8d1e0db4861ad
Class01 <Class01@DESKTOP-1KRANK2>
2023-06-08 15:18:44 +0900

    Initial commit

---
+++ Programmer/sql/select/Select_19.sql
@@ -0,0 +1,31 @@
+
+-- case 1
+SELECT NAME
+from (
+    select *
+    from ANIMAL_INS 
+    order by DATETIME
+)
+where rownum<=1
+
+
+-- case 2
+SELECT NAME 
+from ANIMAL_INS 
+where DATETIME = (
+	select min(DATETIME) 
+	from ANIMAL_INS
+);
+
+
+-- case 3
+SELECT name
+from(
+    select name, 
+    to_number(to_char(DATETIME, 'YYYY')) as year, 
+    to_number(to_char(DATETIME, 'MM')) as month,
+    to_number(to_char(DATETIME, 'DD')) as day
+    from ANIMAL_INS 
+    order by year, month, day
+    )
+where rownum=1 
